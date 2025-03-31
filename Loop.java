1388ac682de99c61969cb9abd837e80776b02a0f
jchinedu <jchinedu080@gmail.com>
2025-03-31 14:36:45 +0100

    git

---
+++ Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+public static void main(String[] args) {
+ int product = 3;
+while(product <=100) {
+product = 3 * product;
+}
+System.out.printf("the answer is %d%n", product);
+}
+}
