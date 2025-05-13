fb0853d749a104353c47b0f3461993231e6f5490
jchinedu <jchinedu080@gmail.com>
2025-05-13 12:33:11 +0100

    'today'

---
+++ Hardwork/checkerboard4.java
@@ -0,0 +1,22 @@
+ public class checkerboard4 {
+   public static void main(String[] args) {
+	for(int i = 1; i <= 10; i++) {
+	       if(i % 2 ==0) {
+	       System.out.print("* ");
+		}
+		else {
+		System.out.print(" ");
+		}
+		for(int j = 1; j <= 10; j++) {
+		if(j % 2 ==0) {
+		System.out.print(" ");
+		}
+		else
+		{
+		System.out.print("*");
+		} 
+		}
+		System.out.println("");
+	 }
+}
+  }
