aa7e66e05b62f3be4d39e646da4ca717d4c067b8
jchinedu <jchinedu080@gmail.com>
2025-06-21 23:46:42 +0100

    'progress'

---
+++ chapter 9/TestCircleWithStaticMembers.java
@@ -0,0 +1,6 @@
+public class TestCircleWithStaticMembers {
+	public static void main(String[] args) {
+	System.out.println("before creating objects"); 
+	System.out.println("the number of circle object is " + CircleWithStaticMember.numberOfObjects);
+
+	CircleWithStaticMember c1 = new CircleWithStaticMember
