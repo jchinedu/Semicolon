f08a6188799af3228f1605462afa14bbdad62a99
jchinedu <jchinedu080@gmail.com>
2025-06-21 17:46:06 +0100

    'progress'

---
+++ chapter 9/TestPoint2d.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import javafx.geometry.point2d;
+
+public class TestPoint2d {
+ public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter point1 x-, y-coordinates: ");
+	double x1 = input.nextDouble();
+	double y1 = input.nextDouble();
+	System.out.print("Enter point2 x-, y-coordinates: ");
+	double x2 = input.nextDouble();
+	double y2 = input.nextDouble();
