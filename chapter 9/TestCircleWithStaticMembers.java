e1be7fc7f2ab366696f3ade02179f8b0a70e4a01
jchinedu <jchinedu080@gmail.com>
2025-06-22 03:18:09 +0100

    'progress'

--- chapter 9/TestCircleWithStaticMembers.java
+++ chapter 9/TestCircleWithStaticMembers.java
@@ -13,6 +13,6 @@ public class TestCircleWithStaticMembers {
 	System.out.println("/nAfter creating c2 and modifying c1");
 	System.out.println("c1: radius (" + c1.radius + ") and number of circle objects c" + c1.numberOfObjects + ")");
 	System.out.println("c2: radius (" + c2.radius + ") and number of circle objects (" + c2.nnumberOfObjects + ")")
-
-
+}
+}
 	
