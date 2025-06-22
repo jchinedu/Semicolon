c09fc66b890335b39d4b4daab17031225f4a01ec
jchinedu <jchinedu080@gmail.com>
2025-06-22 03:21:17 +0100

    'progress'

--- chapter 9/TestCircleWithStaticMembers.java
+++ chapter 9/TestCircleWithStaticMembers.java
@@ -1,18 +1,18 @@
 public class TestCircleWithStaticMembers {
 	public static void main(String[] args) {
 	System.out.println("before creating objects"); 
-	System.out.println("the number of circle object is " + CircleWithStaticMember.numberOfObjects);
+	System.out.println("the number of circle object is " + CircleWithStaticMembers.numberOfObjects);
 
-	CircleWithStaticMember c1 = new CircleWithStaticMember();
+	CircleWithStaticMembers c1 = new CircleWithStaticMembers();
 	System.out.println("/nAfter creating c1");
 	System.out.println("c1: radius (" + c1.radius + ") and number of circle objects (" + c1.numberOfObjects + ")");
 
-	CircleWithStaticMember c1 = new CircleWithStaticMember(5);
+	CircleWithStaticMembers c1 = new CircleWithStaticMembers(5);
 	c1.radius = 9;
 
 	System.out.println("/nAfter creating c2 and modifying c1");
 	System.out.println("c1: radius (" + c1.radius + ") and number of circle objects c" + c1.numberOfObjects + ")");
-	System.out.println("c2: radius (" + c2.radius + ") and number of circle objects (" + c2.nnumberOfObjects + ")")
+	System.out.println("c2: radius (" + c2.radius + ") and number of circle objects (" + c2.numberOfObjects + ")");
 }
 }
 	
