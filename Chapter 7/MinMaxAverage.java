3e3b8e36e62affa72c10b2b1bb361b2ba88e4974
jchinedu <jchinedu080@gmail.com>
2025-07-10 00:10:31 +0100

    ;progress'

--- Chapter 7/MinMaxAverage.java
+++ Chapter 7/MinMaxAverage.java
@@ -1,2 +1,10 @@
 public class MinMaxAverage{
 public static void main(String[] args){
+   if (args.length == 0){
+     System.out.print("no argument entered");
+	return;
+}
+	try{
+	int first = Integer.parseInt(arg[0]);
+	int min = first;
+	int max = first;
