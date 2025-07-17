9cc025a198c5866fcbc8442d60c7ef239cbccdac
jchinedu <jchinedu080@gmail.com>
2025-07-17 23:08:01 +0100

    'progress'

--- Desktop/My Project/MY DIARY/src/Diary/MainApplication.java
+++ Desktop/My Project/MY DIARY/src/Diary/MainApplication.java
@@ -7,5 +7,6 @@ public class MainApplication {
         Diary johnDiary = diaries.findByUsername("john");
         johnDiary.unlockDiary("1234");
         johnDiary.CreateEntry("My First Entry", "Today was amazing.");
+        Entry entry = johnDiary.findEntryById(1);
     }
 }
