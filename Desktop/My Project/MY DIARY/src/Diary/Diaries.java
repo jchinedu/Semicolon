03838b8ed2289c7cc10fcb516cee318fcb4c0006
jchinedu <jchinedu080@gmail.com>
2025-07-17 23:03:10 +0100

    'progress'

--- Desktop/My Project/MY DIARY/src/Diary/Diaries.java
+++ Desktop/My Project/MY DIARY/src/Diary/Diaries.java
@@ -6,5 +6,28 @@ import java.util.List;
 public class Diaries {
     private List<Diary> diaries = new ArrayList<>();
     public void Add(String username, String password) {
+        diaries.add(new Diary(username, password));
     }
+
+    public int size() {
+        return diaries.size();
+    }
+
+    public Diary findByUsername(String username) {
+        for (Diary diary : diaries) {
+            if (diary.getName().equalsIgnoreCase(username)) {
+                return diary;
+            }
+        }
+        return null;
+    }
+
+    public void delete(String username, String password) {
+        for (Diary diary : diaries) {
+            if (diary.getName().equals(username) && diary.getPassword().equals(password)) {
+                diaries.remove(diary);
+                break;
+            }    }
+    }
+
 }
--- Desktop/My Project/MY DIARY/test/DairyTest/testDiaries.java
+++ Desktop/My Project/MY DIARY/test/DairyTest/testDiaries.java
@@ -1,12 +1,35 @@
 package DairyTest;
 
 import Diary.Diaries;
+import Diary.Diary;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class testDiaries {
     @Test
     public void testThatDiaresCanAdd(){
         Diaries diaries = new Diaries();
         diaries.Add("john", "1234");
+        assertEquals(1,diaries.size());
+        diaries.Add("daniel", "2483");
+        assertEquals(2,diaries.size());
     }
-}
+    @Test
+    public void testThatDiariesCanSearchandfind(){
+        Diaries diaries = new Diaries();
+        diaries.Add("john", "1234");
+        Diary result = diaries.findByUsername("john");
+        assertNotNull(result);
+        assertEquals("john", result.getName());
+        }
+    @Test
+    public void testThatDiariesCanDelete(){
+        Diaries diaries = new Diaries();
+        diaries.Add("john", "1234");
+        diaries.delete("john","1234");
+        assertNull(diaries.findByUsername("john"));
+    }
+
+
+    }
