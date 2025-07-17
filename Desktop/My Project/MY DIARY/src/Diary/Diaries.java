4218bb39619f536b0fb85074c9d9850abf178073
jchinedu <jchinedu080@gmail.com>
2025-07-17 22:30:53 +0100

    'done'

---
+++ Desktop/My Project/MY DIARY/src/Diary/Diaries.java
@@ -0,0 +1,10 @@
+package Diary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Diaries {
+    private List<Diary> diaries = new ArrayList<>();
+    public void Add(String username, String password) {
+    }
+}
--- Desktop/My Project/MY DIARY/test/DairyTest/testDiaries.java
+++ Desktop/My Project/MY DIARY/test/DairyTest/testDiaries.java
@@ -1,7 +1,12 @@
 package DairyTest;
 
+import Diary.Diaries;
 import org.junit.jupiter.api.Test;
 
 public class testDiaries {
     @Test
+    public void testThatDiaresCanAdd(){
+        Diaries diaries = new Diaries();
+        diaries.Add("john", "1234");
+    }
 }
