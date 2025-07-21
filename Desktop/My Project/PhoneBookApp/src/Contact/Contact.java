51c24f2e7090228f4d5b336cc8fbc1bcd7770ca5
jchinedu <jchinedu080@gmail.com>
2025-07-21 01:23:09 +0100

    'done'

---
+++ Desktop/My Project/PhoneBookApp/src/Contact/Contact.java
@@ -0,0 +1,12 @@
+package Contact;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ Desktop/My Project/PhoneBookApp/test/TestContact/TestContact.java
@@ -0,0 +1,16 @@
+package TestContact;
+import Contact.Contact;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestContact {
+
+    @Test
+    public void testContactCreation() {
+        Contact mycontact = new Contact("john", "Chinedu", "07046591103");
+
+    }
+
+
+}
