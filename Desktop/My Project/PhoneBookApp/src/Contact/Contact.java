7ba670481126ac927797f6484146edb86145470d
jchinedu <jchinedu080@gmail.com>
2025-07-21 01:47:02 +0100

    'progress'

--- Desktop/My Project/PhoneBookApp/src/Contact/Contact.java
+++ Desktop/My Project/PhoneBookApp/src/Contact/Contact.java
@@ -19,6 +19,21 @@ public class Contact {
     }
 
     public String getPhoneNumber() {
+        if (!phoneNumber.startsWith("0") || phoneNumber.length() != 11) {
+            throw new PhoneNumberMustBeEleven();
+        }
+
         return phoneNumber;
+    }
+
+    public void setPhoneNumber(String number) {
+        this.phoneNumber = number;
+    }
+
+    public void setLastName(String LastName) {
+        this.lastName = LastName;
+    }
+    public void setFirstName(String FirstName) {
+        this.firstName = FirstName;
     }
 }
--- Desktop/My Project/PhoneBookApp/test/TestContact/TestContact.java
+++ Desktop/My Project/PhoneBookApp/test/TestContact/TestContact.java
@@ -1,8 +1,10 @@
 package TestContact;
 import Contact.Contact;
 import org.junit.jupiter.api.Test;
+import Contact.PhoneNumberMustBeEleven;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestContact {
 
@@ -13,6 +15,23 @@ public class TestContact {
         assertEquals("Chinedu", mycontact.getLastName());
         assertEquals("07046591103", mycontact.getPhoneNumber());
     }
-
+    @Test
+    public void testthatphoneNumberMustBeTenDigitLong(){
+        Contact mycontact = new Contact("john", "Chinedu", "0704659110");
+        assertThrows(PhoneNumberMustBeEleven.class, () -> mycontact.getPhoneNumber());
     }
+    @Test
+    public void testSetters() {
+        Contact mycontact = new Contact("daniel", "chima", "08036030275");
+        mycontact.setFirstName("Jane");
+        mycontact.setLastName("Smith");
+        mycontact.setPhoneNumber("08061328142");
+
+        assertEquals("Jane", mycontact.getFirstName());
+        assertEquals("Smith", mycontact.getLastName());
+        assertEquals("08061328142", mycontact.getPhoneNumber());
+    }
+
+
+}
 
