f2895377e154276ffe21b677822c7b601bea7fcf
jchinedu <jchinedu080@gmail.com>
2025-07-29 21:10:35 +0100

    'done'

--- chapter 8/PangramChecker.java
+++ chapter 8/PangramChecker.java
@@ -1,8 +1,9 @@
 public class PangramChecker {
     public boolean check(String sentence) {
         sentence = sentence.toLowerCase(); 
-          boolean[] seen = new boolean[26]; 
-
+          boolean[] seen = new boolean[26];
+For (int i = 0; i < sentence.length(); i++) {
+            
         
 }
 
