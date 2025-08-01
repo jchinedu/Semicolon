a67d14c66f561f989908f6c75d3f4a3704dca4f9
jchinedu <jchinedu080@gmail.com>
2025-08-01 12:55:24 +0100

    'progress'

--- chapter 8/PangramChecker.java
+++ chapter 8/PangramChecker.java
@@ -2,7 +2,7 @@ public class PangramChecker {
     public boolean check(String sentence) {
         sentence = sentence.toLowerCase(); 
           boolean[] seen = new boolean[26];
-For (int i = 0; i < sentence.length(); i++) {
+for (int i = 0; i < sentence.length(); i++) {
 char c = sentence.charAt(i);
  if (c >= 'a' && c <= 'z') {
  seen[c - 'a'] = true; 
