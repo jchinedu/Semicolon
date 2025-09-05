87466cab1d53fe70b6b6a7e5146c8ef3572dc024
jchinedu <jchinedu080@gmail.com>
2025-09-05 14:38:06 +0100

    done

--- PycharmProjects/OnlineCourse/school_system.py
+++ PycharmProjects/OnlineCourse/school_system.py
@@ -4,27 +4,29 @@ class OnlineCourse:
         self.instructor = instructor
         self.students = []
 
-    def enroll_students(self, name):
+    def enroll_students(self, student):
         self.students.append(name)
-        print(f"{name} has been enrolled in the {self.course} course")
+        print(f"{student.name} has been enrolled in the {self.course} course")
 
     def course_details(self):
         print(f"course: {self.course}")
         print(f"instructor: {self.instructor}")
-        print(f"Enrolled students:{','.join (self.students)}")
+        print(f"Enrolled students:")
+        for student in self.students:
+            print(student.name)
 
 
     def completed_course(self, name):
-        if name in self.students:
-            self.students.remove(name)
-            print(f"{name} has been completed {self.course} course")
+        for student in self.students:
+            if student.name in name:
+             self.students.remove(student)
+             print(f"{name} has been completed {self.course} course")
+        print(f"{name} has not been enrolled in the {self.course} course")
 
-        else:
-            print(f"{name} has not been enrolled in the {self.course} course")
-
-    def avg_grade(self,grades):
-        total = sum(grades)
-        average = total / len(grades)
+    def avg_grade(self, grades):
+        numeric_grades = [float(grade) for grade in grades]
+        total = sum(numeric_grades)
+        average = total / len(student.grades)
         print(f"the average grade is: {average}")
 
 
@@ -35,9 +37,31 @@ student = input("enter a student name : ").lower()
 
 
 course = OnlineCourse(course_input, name)
-grades = [90, 85, 92, 78, 80]
-course.avg_grade(grades)
-course.enroll_students(name)
+# grades = [90, 85, 92, 78, 80]
+# course.avg_grade(grades)
+# course.enroll_students(name)
+# course.course_details()
+# course.completed_course("josh")
+
+class Student:
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+non_students = int(input("enter number of students: "))
+
+for _ in range(non_students):
+    student_name = input("enter a student name: ").lower()
+    grades = []
+    for _ in range(3):
+        grade = input("enter a grade: ")
+        grades.append(grade)
+    student = Student(student_name, grades)
+    course.enroll_students(student)
+    course.avg_grade(grades)
 course.course_details()
-course.completed_course("josh")
+
+
+
+
 
